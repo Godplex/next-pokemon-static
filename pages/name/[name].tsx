01180ccb0ecf850c04fba8b495f115a05c82c73d
Julian Rincon <julian1918@hotmail.es>
2022-12-21 16:17:15 -0500

    Incremental static

--- pages/name/[name].tsx
+++ pages/name/[name].tsx
@@ -125,10 +125,22 @@ export const getStaticPaths: GetStaticPaths = async () => {
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { name } = params as { name: string };
 
+  const pokemon = await getPokemonInfo(name);
+
+  if (!pokemon) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
   return {
     props: {
-      pokemon: await getPokemonInfo(name),
+      pokemon,
     },
+    revalidate: 86400, //Actualización cada 24 horas
   };
 };
 
